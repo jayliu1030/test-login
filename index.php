f8bb7c7c6e0c17916b6e87fce9cbfaf084cd2e64
jayliu1030 <yatizliu@gmail.com>
2020-08-13 19:32:02 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php include_once(“home.html”); ?>
+<?php
+
+// using ldap bind
+$ldaprdn  = 'uname';     // ldap rdn or dn
+$ldappass = 'password';  // associated password
+
+// connect to ldap server
+$ldapconn = ldap_connect("ldap://ldap.example.com")
+    or die("Could not connect to LDAP server.");
+
+if ($ldapconn) {
+
+    // binding to ldap server
+    $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+
+    // verify binding
+    if ($ldapbind) {
+        echo "LDAP bind successful...";
+    } else {
+        echo "LDAP bind failed...";
+    }
+
+}
+
+?>
